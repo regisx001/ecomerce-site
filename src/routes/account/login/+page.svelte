4dc7bc9f8c86b3ac9e5523351bee037a5eea4cfa
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2024-07-09 18:03:10 +0100

    initial

---
+++ src/routes/account/login/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { Button } from '@ui/button';
+	import { Input } from '@ui/input';
+	import { Label } from '@ui/label';
+</script>
+
+<div class="h-screen w-full lg:grid lg:grid-cols-2">
+	<div class="hidden bg-muted lg:block">
+		<img
+			src="/Pic/auth.png"
+			alt="placeholder"
+			width="1920"
+			height="1080"
+			class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+		/>
+	</div>
+	<div class="flex items-center justify-center py-12">
+		<div class="mx-auto grid w-[350px] gap-6">
+			<div class="grid gap-2 text-center">
+				<h1 class="text-2xl font-bold">Login</h1>
+				<p class="text-balance text-muted-foreground">
+					Enter your email below to login to your account
+				</p>
+			</div>
+			<div class="grid gap-4">
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input id="email" type="email" placeholder="m@example.com" required />
+				</div>
+				<div class="grid gap-2">
+					<div class="flex items-center">
+						<Label for="password">Password</Label>
+						<a href="/" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
+					</div>
+					<Input id="password" type="password" required />
+				</div>
+				<Button type="submit" class="w-full">Login</Button>
+				<Button variant="outline" class="w-full">Login with Google</Button>
+			</div>
+			<div class="mt-4 text-center text-sm">
+				Don&apos;t have an account?
+				<a href="/signup" class="underline"> Sign up </a>
+			</div>
+		</div>
+	</div>
+</div>
